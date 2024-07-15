18c6aafab98baf583808b29dd8ec2bf740aa2f5f
Learnpro1122 <learningprogramming24@gmail.com>
2024-07-15 11:47:19 +0530

    Update string_swapping.py

--- string_swapping.py
+++ string_swapping.py
@@ -1,4 +1,3 @@
-<<<<<<< HEAD
 '''
 a function to print the combinations of the given string input by the user. 
 
@@ -6,74 +5,39 @@ algorithm or logic:
             the swapping will be started from the last and last 2nd character.
             and will go on till the first and second charcater,
             and will stop when the original string will be combined again...
-ABC
 ACB
 CAB
 CBA
 BCA
-BAC         
+BAC
+ABC         
 '''
+
 
 # ask for string and convert the string into the list for easy swapping 
 string = str(input('enter the string: '))
 list_o = list(string)
 
 #index variable for the string 
-no = len(list_o) - 1
+no = len(list_o) 
 print(no)
 
 # factorial will be needed as to print combindations factorial time.
 fact =  1
-for i in range(1, no+2):
+for i in range(1, no+1):
     fact = fact * i
 print(fact)
-# variable for changing the index of the charcter that is to be swaapee...
+# variable for changing the index of the charcter that is to be swap...
 n = 1
+
+print(list_o[no-n-1])
 # swapping from last 
 for k in range(fact):
-    for i in list_o:
-        list_o[no-n-1] , list_o[no-n] = list_o[no-n] ,list_o[no-n-1] #variables n,no are to be changed....
+    for i in list_o:     
+        list_o[no-n-1] , list_o[no-n] = list_o[no-n] ,list_o[no-n-1] 
+        n = n + 1
         print(list_o)
-    if n ==  no :
+        if n ==  no :
+            n = 1
         break
-    n = n + 1
-=======
-'''
-a function to print the combinations of the given string input by the user. 
 
-algorithm or logic:
-            the swapping will be started from the last and last 2nd character.
-            and will go on till the first and second charcater,
-            and will stop when the original string will be combined again...
-ABC
-ACB
-CAB
-CBA
-BCA
-BAC         
-'''
-
-# ask for string and convert the string into the list for easy swapping 
-string = str(input('enter the string: '))
-list_o = list(string)
-
-#index variable for the string 
-no = len(list_o) - 1
-print(no)
-
-# factorial will be needed as to print combindations factorial time.
-fact =  1
-for i in range(1, no+2):
-    fact = fact * i
-print(fact)
-# variable for changing the index of the charcter that is to be swaapee...
-n = 1
-# swapping from last 
-for k in range(fact):
-    for i in list_o:
-        list_o[no-n-1] , list_o[no-n] = list_o[no-n] ,list_o[no-n-1] #variables n,no are to be changed....
-        print(list_o)
-    if n ==  no :
-        break
-    n = n + 1
->>>>>>> 48d086b1412589e7e0b1581d4a6fbaf7539cdb56
